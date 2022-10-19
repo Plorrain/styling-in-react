1eae4cffda06e47af66b850c2da1a21aedd22da8
Philippine Lorrain <philippinelorrain@gmail.com>
2022-10-19 17:22:25 -0400

    found the missing files

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -23,8 +23,11 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'green' }}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'orange' : 'green' }}>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler}
+          style={{ borderColor: !isValid ? 'orange' : 'green',
+          background: !isValid ? 'orange' : 'green' }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
